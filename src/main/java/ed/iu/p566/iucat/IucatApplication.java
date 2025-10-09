2090c735a4cb6622c63d08466b5281d50ede0a71
Gauri <gsmarkan@iu.edu>
2025-10-08 20:09:26 -0400

    Initial project setup with Spring Boot

---
+++ src/main/java/ed/iu/p566/iucat/IucatApplication.java
@@ -0,0 +1,13 @@
+package ed.iu.p566.iucat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IucatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IucatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ed/iu/p566/iucat/IucatApplicationTests.java
@@ -0,0 +1,13 @@
+package ed.iu.p566.iucat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IucatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
